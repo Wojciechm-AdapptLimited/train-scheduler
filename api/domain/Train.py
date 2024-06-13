9f18aec01644dce08b0bc30d4d128dd8c422376d
lucapl <lukasz.andryszewski@outlook.com>
2024-06-13 17:40:04 +0200

    Initial request train

--- api/domain/Train.py
+++ api/domain/Train.py
@@ -3,20 +3,24 @@ import math
 import random
 import threading
 import time
-import cassandra
-import cassandra.cluster
+import requests
 
 # import asyncio
 # import websockets
 
 
 class Train(abc.ABC, threading.Thread):
-    def __init__(self, train_id, delay=None, max_delay=4):
+    def __init__(self, train_id, delay=None, max_delay=4, repeats=None):
         super(Train, self).__init__()
         self.train_id = train_id
         self.running = True
         self._delay = delay
         self.max_delay = max_delay
+        self.repeats = repeats
+    
+    @property
+    def perpetual(self):
+        return self.repeats is None
 
     @abc.abstractmethod
     def generate_info(self) -> dict[str, float]:
@@ -36,19 +40,27 @@ class Train(abc.ABC, threading.Thread):
     def delay(self) -> float:
         return self._delay or random.uniform(0, self.max_delay)
 
+    def train_iter(self) -> None:
+        info = self.generate_info()
+        time.sleep(self.delay)
+        self.send_info(info)
+
     def start(self) -> None:
-        while self.running:
-            info = self.generate_info()
-            time.sleep(self.delay)
-            self.send_info(info)
+        if self.perpetual:
+            while self.running:
+                self.train_iter()
+            return None
+        
+        for i in range(self.repeats):
+            self.train_iter()
 
     def stop(self) -> None:
         self.running = False
 
 
-class TestTrain(Train):
+class PositionTrain(Train):
     def __init__(self, train_id, delay=None, max_delay=4):
-        super(TestTrain, self).__init__(train_id, delay, max_delay)
+        super(PositionTrain, self).__init__(train_id, delay, max_delay)
         self.t = 0
         self.poland_coords = {
             "up-left": [53.8014, 14.9841],
@@ -78,14 +90,23 @@ class TestTrain(Train):
 #cluster_ips = [f'{server_ip}:{port}' for port in ports]
 
 
-class SocketTrain(TestTrain):
-    def __init__(self, train_id, delay=None, max_delay=4):
-        super(SocketTrain, self).__init__(train_id, delay, max_delay)
-        self.db = cassandra.cluster.Cluster(
-            [f"127.0.0.{i}" for i in range(1, 4)],
-            port=9042)
-        self.session = self.db.connect('train')
+class RequestTrain(PositionTrain):
+    def __init__(self, train_id, delay=None, max_delay=4, repeats=None):
+        super(RequestTrain, self).__init__(train_id, delay, max_delay, repeats)
+        self.url = "127.0.0.1/"
 
     def send_info(self, info):
-        query = f'INSERT INTO trains (train_id, x, y) VALUES ({self.train_id},{info.x},{info.y})'
-        self.session.execute(query)
+        _ = requests.post(self.url+"train/position", json={
+            "id": self.train_id,
+            "x": info.x,
+            "y": info.y
+        })
+
+    #     self.db = cassandra.cluster.Cluster(
+    #         [f"127.0.0.{i}" for i in range(1, 4)],
+    #         port=9042)
+    #     self.session = self.db.connect('train')
+
+    # def send_info(self, info):
+    #     query = f'INSERT INTO trains (train_id, x, y) VALUES ({self.train_id},{info.x},{info.y})'
+    #     self.session.execute(query)
