ad029d68697bef8ed6c2b2406bc274f12b19bb99
Wojciechm-AdapptLimited <wojciech.maczka@protonmail.com>
2024-06-14 19:29:02 +0200

    feat(api) add db seeding for cassandra

--- scripts/seed_db.py
+++ scripts/seed_db.py
@@ -1,49 +1,40 @@
-import psycopg2
+from datetime import datetime
+from cassandra.cqlengine import connection
+from cassandra.cqlengine.management import sync_table
+from api.domain import Train, Seat, Passenger, Ticket
 
-conn = psycopg2.connect(
-    "dbname=trains user=postgres password=1234 host=localhost port=5432"
+connection.setup(
+    hosts=["127.0.0.1"],
+    default_keyspace="ttms",
+    consistency=connection.ConsistencyLevel.QUORUM,
 )
 
-cur = conn.cursor()
+sync_table(Train)
+sync_table(Seat)
+sync_table(Passenger)
+sync_table(Ticket)
 
-cur.execute("CREATE TABLE train (id serial PRIMARY KEY, name VARCHAR);")
-
-cur.execute(
-    "CREATE TABLE seat (id serial PRIMARY KEY, train INTEGER, cart INTEGER, seat_number INTEGER, FOREIGN KEY (train) REFERENCES train(id));"
-)
-
-cur.execute("CREATE TABLE customer (login VARCHAR PRIMARY KEY);")
-
-cur.execute(
-    "CREATE TABLE schedule (id serial PRIMARY KEY, train INTEGER, departure_time TIMESTAMP,  arrival_time TIMESTAMP, FOREIGN KEY (train) REFERENCES train(id));"
-)
-
-cur.execute(
-    "CREATE TABLE ticket (id serial PRIMARY KEY, customer VARCHAR, seat INTEGER, schedule INTEGER, FOREIGN KEY (customer) REFERENCES customer(login), FOREIGN KEY (seat) REFERENCES seat(id), FOREIGN KEY (schedule) REFERENCES schedule(id));"
-)
+stations = ["Poznan", "Krakow"]
 
 for i in range(1, 11):
-    cur.execute(f"INSERT INTO train (name) VALUES ('train_{i}');")
+    departure = datetime(2024, 1, i % 5 + 1, i % 3 + 4, 0, 0)
+    arrival = datetime(2024, 1, i % 5 + 1, i % 3 + 10, 0, 0)
+    from_station = stations[i % 2]
+    to_station = stations[(i + 1) % 2]
+    Train.create(
+        id=i,
+        departure=departure,
+        arrival=arrival,
+        from_station=from_station,
+        to_station=to_station,
+    )
+
 
 for i in range(1, 11):
-    for j in range(1, 11):
+    for j in range(1, 6):
         for k in range(1, 61):
-            cur.execute(
-                f"INSERT INTO seat (train, cart, seat_number) VALUES ({i}, {j}, {k});"
-            )
-
-for i in range(1, 11):
-    for j in range(1, 10):
-        date = i if i > 9 else f"0{i}"
-
-        cur.execute(
-            f"INSERT INTO schedule (train, departure_time, arrival_time) VALUES ({i}, '2024-01-{date} 0{j}:00:00', '2024-01-{date} 1{j}:00:00');"
-        )
-
-for i in range(1, 5):
-    cur.execute(f"INSERT INTO customer (login) VALUES ('customer_{i}');")
+            Seat.create(train=i, seat=f"{j}-{k}", occupied=False)
 
 
-cur.close()
-conn.commit()
-conn.close()
+Passenger.create(login="c1")
+Passenger.create(login="c2")
